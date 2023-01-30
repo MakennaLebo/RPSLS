3f1bf77a4caef167fe0d82a13b5e483f2bad8f11
MakennaLebo <kennalebo@hotmail.com>
2023-01-30 09:46:09 -0500

    Working Rock Paper Scissors Game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [userChoice, setUserChoice] = useState(null)
+  const [computerChoice, setComputerChoice] = useState(null)
+  const [result, setResult] = useState(null)
+  const choices = ['paper', 'rock', 'scissors', 'lizard', 'spock'];
+
+  const handleClick = (value) => {
+    setUserChoice(value)
+    getComputerChoice()
+  }
+
+  const getComputerChoice = () => {
+    const compChoice = choices[Math.floor(Math.random() * choices.length)]
+    setComputerChoice(compChoice);
+  }
+
+  useEffect (() => {
+   {
+    switch(userChoice + computerChoice) {
+      case 'scissorspaper':
+      case 'rockscissors':
+      case 'paperrock':
+      case 'lizardspock':
+      case 'spockscissors':
+      case 'paperspock':
+      case 'scissorslizard':
+      case 'spockrock':
+      case 'rocklizard':
+      case 'lizardpaper':
+          setResult('YOU WIN!')
+          break
+      case 'paperscissors':
+      case 'paperlizard':
+      case 'scissorsspock':
+      case 'scissorsrock':
+      case 'spocklizard':
+      case 'spockpaper':
+      case 'rockspock':
+      case 'rockpaper':
+      case 'lizardrock':
+      case 'lizardscissors':
+        setResult ('YOU LOSE!')
+        break
+      case 'paperpaper':
+      case 'rockrock':
+      case 'scissorsscissors':
+      case 'spockspock':
+      case 'lizardlizard':
+       setResult ('DRAW!')
+       break
+    }
+  }
+  }, [computerChoice, userChoice])
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>User Choice is : {userChoice}</h1> 
+     <h1>Computer Choice is : {computerChoice}</h1> 
+
+     {choices.map((choice, index) => 
+     <button key={index} onClick={() => handleClick(choice)}>{choice} </button>
+     )}
+
+     <h1>{result}</h1>
     </div>
   );
 }
 
-export default App;
+export default App
